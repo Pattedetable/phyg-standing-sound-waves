1e5ff3722fa83b88d896d826e1e5f54ee13eda3b
Manuel Barrette <ragnathok@gmail.com>
2017-09-03 15:37:05 -0400

    Mis à jour la boîte de dialogue / Updated dialog

--- dialog.py
+++ dialog.py
@@ -11,11 +11,17 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(244, 180)
-        Dialog.setWindowModality(2)
+        Dialog.setWindowModality(QtCore.Qt.ApplicationModal)
+        Dialog.resize(497, 168)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
+        Dialog.setSizePolicy(sizePolicy)
         self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
         self.verticalLayout.setObjectName("verticalLayout")
         self.label = QtWidgets.QLabel(Dialog)
+        self.label.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByMouse|QtCore.Qt.TextSelectableByKeyboard|QtCore.Qt.TextSelectableByMouse)
         self.label.setObjectName("label")
         self.verticalLayout.addWidget(self.label)
         self.pushButton = QtWidgets.QPushButton(Dialog)
@@ -29,5 +35,5 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "À propos"))
-        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-weight:600;\">Auteur :</span> Manuel Barrette</p><p><span style=\" font-weight:600;\">Année :</span> 2017</p><p><span style=\" font-weight:600;\">Code source :</span></p><p><span style=\" font-weight:600;\">Licence :</span> GNU GPLv3</p></body></html>"))
+        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-weight:600;\">Auteur :</span> Manuel Barrette</p><p><span style=\" font-weight:600;\">Année :</span> 2017</p><p><span style=\" font-weight:600;\">Code source :</span> https://github.com/Pattedetable/onde-sonore-stationnaire</p><p><span style=\" font-weight:600;\">Licence :</span> GNU GPLv3</p></body></html>"))
         self.pushButton.setText(_translate("Dialog", "Fermer"))
