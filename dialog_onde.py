7dfb55d8b9c673f66439a25362155a6438775205
Manuel Barrette <ragnathok@gmail.com>
2022-08-13 21:22:54 -0400

    Updated years

--- dialog_onde.py
+++ dialog_onde.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2017-2020 Manuel Barrette
+# Copyright 2017-2022 Manuel Barrette
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
